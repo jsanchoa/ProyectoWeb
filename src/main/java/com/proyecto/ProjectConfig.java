4eb3e0e0053e7e6fd6af630dc47a169212ec1b61
jtorres90443 <jtorres90443@ufide.ac.cr>
2025-04-21 21:37:04 -0600

    Mini cambio

--- src/main/java/com/proyecto/ProjectConfig.java
+++ src/main/java/com/proyecto/ProjectConfig.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.proyecto;
 
 import org.springframework.context.annotation.Bean;
@@ -15,10 +11,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-/**
- *
- * @author naoal
- */
 @Configuration
 public class ProjectConfig implements WebMvcConfigurer{
     @Override
